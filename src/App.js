64e5a8fb48824688dc5f9661765d1e8812460d78
Vaishnavi Pawar <ms.vaishnavipawar@gmail.com>
2021-07-08 21:26:07 +0530

    overlay and autoplay added

--- src/App.js
+++ src/App.js
@@ -8,37 +8,84 @@ import "swiper/components/effect-coverflow/effect-coverflow.min.css"
 import "swiper/components/pagination/pagination.min.css"
 import "./styles.css";
 
-
 // import Swiper core and required modules
 import SwiperCore, {
-  EffectCoverflow,Pagination
+  EffectCoverflow,Pagination,Autoplay
 } from 'swiper/core';
 
 // install Swiper modules
-SwiperCore.use([EffectCoverflow,Pagination]);
-
+SwiperCore.use([EffectCoverflow,Pagination,Autoplay]);
 
 export default function App() {
-  
+
   return (
+    
     <>
-    <Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={'auto'} loop={true} coverflowEffect={{
-  "rotate": 50,
+    <Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={'auto'} loop={true} 
+      autoplay={{ delay: 2000 }}
+    coverflowEffect={{
+  "rotate": 60,
   "stretch": 0,
   "depth": 100,
   "modifier": 1,
   "slideShadows": true
 }} pagination={true} className="mySwiper">
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide>
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-1.jpg" class="image" />
+    <div class="overlay">Event 1</div>
+    </div>  
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-2.jpg" class="image" />
+    <div class="overlay">Event 2</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-3.jpg" class="image" />
+    <div class="overlay">Event 3</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-4.jpg" class="image" />
+    <div class="overlay">Event 4</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-5.jpg" class="image" />
+    <div class="overlay">Event 5</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-6.jpg" class="image" />
+    <div class="overlay">Event 6</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-7.jpg" class="image" />
+    <div class="overlay">Event 7</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-8.jpg" class="image" />
+    <div class="overlay">Event 8</div>
+    </div> 
+  </SwiperSlide>
+  <SwiperSlide>
+    <div class="container">
+    <img src="https://swiperjs.com/demos/images/nature-9.jpg" class="image" />
+    <div class="overlay">Event 9</div>
+    </div>
+  </SwiperSlide>
   </Swiper>
     </>
   )
+  
 }
